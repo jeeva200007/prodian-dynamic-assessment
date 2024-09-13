33609c78c6d6ae4761645c2e6648c55481245e6c
Jeeva <jeevaaru2000@gmail.com>
2024-09-13 11:09:23 +0530

    cleared-bottom scroll and implemented search

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-
 const toggle = document.querySelector(".toggle")
 const elements = document.querySelectorAll('.link-elements')
 const heroContent = document.querySelector('.hero-content'); 
+const searchBtn = document.querySelector('.search-btn')
 
+const dataArray = []
 
 toggle.addEventListener('click', ()=> {
     elements.forEach(element => {
@@ -17,6 +18,8 @@ toggle.addEventListener('click', ()=> {
 })
 
 
+
+
 async function getData() {
     try {
         const res = await fetch('data.json');
@@ -24,7 +27,10 @@ async function getData() {
             console.log('Response not found');
         }
         const someData = await res.json();
-        
+        if(someData){
+            dataArray.push(someData.data)
+        }
+
         someData.data.map((data, id) => {
             const card = document.createElement('div');
             card.classList.add('cards');
@@ -46,6 +52,35 @@ async function getData() {
         console.error(error);
     }
 }
+function getDisplayData(data){
+    console.log(data)
+    heroContent.innerHTML = ''
+     data.map((item,id)=>{ 
+        const card = document.createElement('div');
+            card.classList.add('cards');
+            card.setAttribute('data-aos', 'fade-up');
+            card.innerHTML = `
+                <div class="text-content" key = ${id}>
+                    <h1>${item.title}</h1>
+                    <h3>${item.subtitle}</h3>
+                    <div class="btns">
+                        <button>Download</button>
+                        <button>Share</button>
+                    </div>
+                </div>
+                <img src="${item.image}" alt="${data.title}">
+            `;
+            heroContent.appendChild(card);
+    })
+}
+// console.log(dataArray)
+function filterBlogs() {
+    const input = document.querySelector('.input').value.toLowerCase()
+    const filterValues = dataArray[0].filter(item => item.title.toLowerCase().includes(input) || item.subtitle.toLowerCase().includes(input) )
+    getDisplayData(filterValues)
+}
+
+searchBtn.addEventListener('click', filterBlogs)
 
 
 
